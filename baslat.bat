8249c690c8ad4961e3fa198ec47e3ce5fd25e957
Yiğit Duman <yigitq@gmail.com>
2019-01-29 17:01:00 +0300

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,4 @@
+@echo off
+:baslat
+MinecraftClient.exe UnioGuvenlik01 -
+GOTO baslat
---
+++ baslat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HesapAdi="UnioGuvenlik01"
+
+while true ; do
+echo "Başlatmayı engellemek için CTRL + C yapın."
+echo "UnioCraft 3 saniye içinde açılıyor..."
+sleep 1
+echo "UnioCraft 2 saniye içinde açılıyor..."
+sleep 1
+echo "UnioCraft 1 saniye içinde açılıyor..."
+sleep 1
+
+/usr/bin/mono MinecraftClient.exe $HesapAdi -
+done
